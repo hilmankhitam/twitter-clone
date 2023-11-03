9746d5a73125dbb83333ae0a4e0ada36b578ee12
hilman khitam <hilmankhitam100@gmail.com>
2023-11-03 22:42:47 +0800

    users & detail profile

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,14 @@
 import bcrypt from 'bcrypt';
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 import prisma from '@/libs/prismadb';
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         CredentialsProvider({
-            id: 'credentials',
             name: 'credentials',
             credentials: {
                 email: { label: 'email', type: 'text' },
@@ -51,4 +50,6 @@ export default NextAuth({
         secret: process.env.NEXTAUTH_JWT_SECRERT
     },
     secret: process.env.NEXTAUTH_SECRET
-})
+};
+
+export default NextAuth(authOptions);
--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,21 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import serverAuth from "@/libs/serverAuth";
+import prisma from '@/libs/prismadb';
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse,
 ) {
     if (req.method !== 'GET') {
         return res.status(405).end();
     }
 
     try {
-        const { currentUser } = await serverAuth(req, res);
+        const users = await prisma.user.findMany({
+            orderBy: {
+                createdAt: 'desc',
+            }
+        });
 
-        return res.status(200).json(currentUser);
+        return res.status(200).json(users);
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return res.status(400).end();
     }
 }
