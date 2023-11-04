511f8815b2f4e010e32c4c5e1173543eb5dec9e9
hilman khitam <hilmankhitam100@gmail.com>
2023-11-04 10:56:37 +0800

    edit user & image upload

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -5,6 +5,7 @@ import { BiCalendar } from 'react-icons/bi';
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 import Button from '../Button';
+import useEditModal from '@/hooks/useEditModal';
 
 interface UserBioProps {
     userId: string;
@@ -15,6 +16,8 @@ const UserBio: React.FC<UserBioProps> = ({
 }) => {
     const { data: currentUser } = useCurrentUser();
     const { data: fetchedUser } = useUser(userId);
+
+    const editModal = useEditModal();
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) {
@@ -28,7 +31,7 @@ const UserBio: React.FC<UserBioProps> = ({
         <div className='border-b-[1px] border-neutral-800 pb-4'>
             <div className="flex justify-end p-2">
                 {currentUser?.id === userId ? (
-                    <Button secondary label='Edit' onClick={() => { }} />
+                    <Button secondary label='Edit' onClick={editModal.onOpen} />
                 ) : (
                     <Button
                         onClick={() => { }}
@@ -43,7 +46,7 @@ const UserBio: React.FC<UserBioProps> = ({
                         {fetchedUser?.name}
                     </p>
                     <p className="text-md text-neutral-500">
-                        {fetchedUser?.username}
+                        @{fetchedUser?.username}
                     </p>
                 </div>
                 <div className="flex flex-col mt-4">
